df054824875423b636b6ff640e1e4e83c20e10d2
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-03-16 19:09:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+function nestedTarget(){
+  var pullOut = document.getElementById('nested');
+  return pullOut.querySelector('.target');
+}
+function increaseRankBy(n){
+  const lis = document
+  .getElementById('app')
+  .querySelectorAll('ul.ranked-list li');
 
+for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+}
+}
+function deepestChild(){
+  var pullsOut = document
+  .getElementById('app')
+  .querySelectorAll('div#grand-node div');
+  return pullsOut[3];
+}
