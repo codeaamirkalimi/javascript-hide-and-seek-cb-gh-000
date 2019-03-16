611a48c260b4419e842a17964f26b15290ac8b79
Aamir Kalimi <aamirkalimi.edu@gmail.com>
2019-03-16 18:53:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,11 @@ function nestedTarget(){
   return pullOut.querySelector('.target');
 }
 function increaseRankBy(n){
-
+  const lis = document
+  .getElementById('app')
+  .querySelectorAll('ul.ranked-list li');
+ 
+for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = parseInt((i + n).toString(),10);
+}
 }
